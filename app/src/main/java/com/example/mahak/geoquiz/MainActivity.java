5ed8887bceb45b33b85c2de6175464c1b7d7ed72
mahak garg <mhkgarg@gmail.com>
2016-07-20 22:45:22 +0530

    handles rotation

--- app/src/main/java/com/example/mahak/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/mahak/geoquiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mahak.geoquiz;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,6 +11,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int current =0;
+    String TAG = "MainActivity";
+    String KEY_INDEX = "Index";
+
     Question[] questions = {new Question(R.string.question1,true),
             new Question(R.string.question2,false),
             new Question(R.string.question3,true)};
@@ -18,14 +23,55 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(questions[current].getQuesId());
 
     }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null)
+            current = savedInstanceState.getInt(KEY_INDEX,0);
         changeQuestion();
+        Log.d(TAG,"onCreate");
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        Log.i(TAG,"onSavedInstanceState");
+        savedState.putInt(KEY_INDEX,current);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+
+    }
     public void trueListener(View view){
         if(questions[current].isTrueAnswer())
             Toast.makeText(this,"correct",Toast.LENGTH_SHORT).show();
